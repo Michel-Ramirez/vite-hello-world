b76442b5945ee2eda907dbb4774cc8e2c362d10d
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-06-28 15:49:54 +0200

    add compoment

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
+import AppTitle from './components/AppTitle.vue';
+
 <script>
 export default {
   data(){
     return {
 
     }
+  },
+  component: {
+    AppTitle: AppTitle,
   }
 }
 </script>
 
 <template>
-
+  <AppTitle />
 </template>
 
 <style>
